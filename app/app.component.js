d9202d8bfc12d7e1a66f22688d40746c6f153f95
unknown <satish.dawala@ADMINISTRATOR74.ojasit.in>
2017-11-03 11:39:58 +0530

    adding all files in github

---
+++ app/app.component.js
@@ -0,0 +1,33 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var core_1 = require('@angular/core');
+var AppComponent = (function () {
+    function AppComponent() {
+        this.title = "Welcome to Angular2 App..!! ";
+        this.showElement = true;
+        this.color = 'yellow';
+    }
+    AppComponent.prototype.onClick = function (value) {
+        alert('button clicked..!! ' + value);
+        console.log('button clicked ' + value);
+    };
+    AppComponent = __decorate([
+        core_1.Component({
+            selector: 'my-app',
+            template: "<div>\n  \t\t\t\t<h1>{{title}}</h1>\n  \t\t\t\t<div>\n\t\t\t\tFirstname: <input type=\"text\" #demoinput [(ngModel)]=\"fname\"> <br/><br/>\n\t\t\t\tLastname: <input type=\"text\" #demoinput [(ngModel)]=\"lname\"><br/><br/>\n\t\t\t\t<button (click)=\"onClick(demoinput.value)\">Click</button>\n\t\t\t\t<h3 *ngIf=\"showElement\">Full Name: {{fname}} {{lname}}</h3>\n\t\t\t\t</div>\n\t\t\t\t<div [ngSwitch]='color'>\n\t\t\t\t\t<p *ngSwitchWhen=\"'blue'\">Blue will be print on screen</p>\n\t\t\t\t\t<p *ngSwitchWhen=\"'green'\">Green color will be print on screen</p>\n\t\t\t\t\t<p *ngSwitchDefault>Invalid Color</p>\n\t\t\t\t</div>\n  \t\t\t</div>",
+            styles: ["\n\t\t\t\th3{\n\t\t\t\t\tcolor:#998fbf;\n\t\t\t\t}\n  \t\t\t"]
+        }), 
+        __metadata('design:paramtypes', [])
+    ], AppComponent);
+    return AppComponent;
+}());
+exports.AppComponent = AppComponent;
+//# sourceMappingURL=app.component.js.map
---
+++ app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: `<div>
+  				<h1>{{title}}</h1>
+  				<div>
+				Firstname: <input type="text" #demoinput [(ngModel)]="fname"> <br/><br/>
+				Lastname: <input type="text" #demoinput [(ngModel)]="lname"><br/><br/>
+				<button (click)="onClick(demoinput.value)">Click</button>
+				<h3 *ngIf="showElement">Full Name: {{fname}} {{lname}}</h3>
+				</div>
+				<div [ngSwitch]='color'>
+					<p *ngSwitchWhen="'blue'">Blue will be print on screen</p>
+					<p *ngSwitchWhen="'green'">Green color will be print on screen</p>
+					<p *ngSwitchDefault>Invalid Color</p>
+				</div>
+  			</div>`,
+  styles:[`
+				h3{
+					color:#998fbf;
+				}
+  			`]
+})
+export class AppComponent {
+	public title = "Welcome to Angular2 App..!! "
+	public fname;
+	public lname;
+	public showElement =true;
+	public color = 'yellow';
+	onClick(value){
+
+		alert('button clicked..!! '   +value);
+		console.log('button clicked '   +value);
+	}
+ }
